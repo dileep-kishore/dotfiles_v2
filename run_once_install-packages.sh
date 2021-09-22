a3e49d7cb316b5a22668d85ce65dc7724ef2da4c
Dileep Kishore <k.dileep1994@gmail.com>
2021-09-21 22:00:36 -0400

    Fix: Install doom emacs only if not present

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -133,5 +133,7 @@ if [ ! -d $HOME/.tmux/plugins/tpm ]; then
 fi
 
 # install doom emacs
+if [ ! -f ~/.emacs.d/bin/doom ]; then
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
+fi
