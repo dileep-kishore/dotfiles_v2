5f4ae202461fd0e027889074ef0392d86b53791f
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-17 15:01:24 -0500

    Fix: Wacom rotation and device map

--- bin/wacom_config.sh
+++ bin/wacom_config.sh
@@ -7,15 +7,19 @@ if [[ "$ORIENT" == "right" ]] ; then
     # Stylus
     xsetwacom set 21 MapToOutput 1920x1080+1920+0
     xsetwacom set 21 Area 0 0 21600 13500
+    xsetwacom set 21 Rotate half
     # Eraser
     xsetwacom set 22 MapToOutput 1920x1080+1920+0
-    xsetwacom set 21 Area 0 0 21600 13500
+    xsetwacom set 22 Area 0 0 21600 13500
+    xsetwacom set 22 Rotate half
 elif [[ "$ORIENT" == "left" ]] ; then
     echo "Left orientation"
     # Stylus
     xsetwacom set 21 MapToOutput 1920x1080+0+0
     xsetwacom set 21 Area 0 0 21600 13500
+    xsetwacom set 21 Rotate half
     # Eraser
     xsetwacom set 22 MapToOutput 1920x1080+0+0
-    xsetwacom set 21 Area 0 0 21600 13500
+    xsetwacom set 22 Area 0 0 21600 13500
+    xsetwacom set 22 Rotate half
 fi
