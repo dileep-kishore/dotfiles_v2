1e130f64fe8e22b085aeb977a8c59337e33c35a6
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-20 17:21:56 -0500

    Fix: Issue with formatoptions

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -3,3 +3,7 @@ require("user.keymaps")
 require("user.plugins")
 require("user.impatient")
 require("user.colorscheme")
+
+-- FIXME: These have to be set at end because a plugin is modifying them
+vim.opt.formatoptions:append({"n", "1"})
+vim.opt.formatoptions:remove({"r", "o"})
