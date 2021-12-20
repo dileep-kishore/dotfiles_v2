9d03d41065c4fb352a7db16c590f3cc3096ca93b
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-20 16:13:16 -0500

    Add: nvim plugins for code navigation, git and statusline

--- private_dot_config/nvim/lua/user/plugins.lua
+++ private_dot_config/nvim/lua/user/plugins.lua
@@ -57,25 +57,71 @@ return packer.startup(function(use)
     use "tamago324/nlsp-settings.nvim"                           -- language server settings defined in json
     use "jose-elias-alvarez/null-ls.nvim"                        -- configurations for formatters and linters
     use "folke/trouble.nvim"                                     -- A pretty list for showing diagnostics
+    -- Code navigation
+    use "simrat39/symbols-outline.nvim"                          -- A tree like view for navigating symbols
+    -- Spell check
+    use "lewis6991/spellsitter.nvim"
 
     -- Completions
+    use "windwp/nvim-autopairs"                                  -- Power autopair plugin for Neovim
 
     -- Keybindings
     use "folke/which-key.nvim"                                   -- Displays popup with possible keybindings of command
+    -- Commenting
+    use {
+        "numToStr/Comment.nvim",                                 -- Smart powerful commenting framework
+        config = function()
+            require("Comment").setup()
+        end
+    }
 
     -- Git
     use "TimUntersberger/neogit"                                 -- Magit clone for Neovim
+    use {
+        "pwntester/octo.nvim",                                   -- Edit GitHub issues and pull requests
+        config = function()
+            require"octo".setup()
+        end 
+    }  
     use "lewis6991/gitsigns.nvim"                                -- Show better gitsigns in the gutter
+
+    -- Motions, windows and navigation
+    use "famiu/bufdelete.nvim"                                   -- Delete buffers without messing up window layout
+    -- Terminals
+    use "akinsho/toggleterm.nvim"                                -- Persist and toggle multiple terminals during an editing session
+    -- TMUX Navigation
+    use {
+        "numToStr/Navigator.nvim",                               -- Smoothly navigate between splits and panes
+        config = function()
+            require("Navigator").setup()
+        end
+    }
 
     -- Themes and eye candy
     use "folke/tokyonight.nvim"
     use "EdenEast/nightfox.nvim"
+    -- Tabline
+    use "akinsho/bufferline.nvim"                                -- A snazzy buffer line built using lua
+    -- Statusline
+    use "nvim-lualine/lualine.nvim"                              -- A blazing fast statusline written in lua
     -- Dashboard
+    use {
+        "goolord/alpha-nvim",                                    -- A fast framework for making a greeter
+        config = function ()
+            require"alpha".setup(require"alpha.themes.dashboard".opts)
+        end
+    }
     -- Files and explorer
-    use { "kyazdani42/nvim-tree.lua", config = function() require "nvim-tree".setup {} end }
+    use {
+        "kyazdani42/nvim-tree.lua",
+        config = function()
+            require "nvim-tree".setup {}
+        end
+    }
     use "kyazdani42/nvim-web-devicons"                           -- Nice set of icons for files
     -- Indentation
     use "lukas-reineke/indent-blankline.nvim"                    -- Adds indentation guides to all lines (including blanks)
+
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
