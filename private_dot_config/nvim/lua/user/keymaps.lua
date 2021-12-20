f3386b69f46b48e9c26a03bb1a51b34cddd445e9
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-20 17:41:03 -0500

    Fix: Line move up and down commands

--- private_dot_config/nvim/lua/user/keymaps.lua
+++ private_dot_config/nvim/lua/user/keymaps.lua
@@ -35,8 +35,8 @@ keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 
 -- Turn off highlight after search
 keymap("n", "<Leader>/", ":nohlsearch<CR>", opts)
@@ -55,6 +55,10 @@ keymap("n", "<Leader>q", ":q<CR>", opts)
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
 
+-- Move text up and down
+keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
 
 -- Visual --
 -- Stay in indent mode
@@ -62,8 +66,8 @@ keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
 keymap("v", "p", '"_dP', opts)
 
 
