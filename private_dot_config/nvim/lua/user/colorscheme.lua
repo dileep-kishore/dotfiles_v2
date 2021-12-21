7a1d52e62cea4d71b564a20944a26245fb7444d1
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-21 08:12:45 -0500

    Change: Set default theme to tokyonight in all tools

--- private_dot_config/nvim/lua/user/colorscheme.lua
+++ private_dot_config/nvim/lua/user/colorscheme.lua
@@ -1,9 +1,23 @@
 ---- Tokyo Night
---
+
 -- config
-vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_style = "night"
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 -- load the colorscheme
 vim.cmd[[colorscheme tokyonight]]
 
+
+-- Config nvim-colorizer
+-- neoscroll
+-- vim-illuminate
+-- registers.nvim
+-- neoclip
+-- bufferline.nvim
+-- lualine
+-- nvim-gps
+-- alpha-nvim
+-- nvim-tree
+-- nvim-web-devicons
+-- indent-blankline
+-- TrueZen
