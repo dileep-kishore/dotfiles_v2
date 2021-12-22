7a2d2aa58abf7c47c82eceaa0a0e5b5051bb0f61
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-22 15:01:07 -0500

    Add: Specs.nvim settings

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -16,6 +16,7 @@ require "user.togglterm"
 require "user.indentline"
 require "user.dashboard"
 require "user.whichkey"
+require "user.navigation"
 -- require("user.misc")
 
 -- FIXME: These have to be set at end because a plugin is modifying them
---
+++ private_dot_config/nvim/lua/user/navigation.lua
@@ -0,0 +1,33 @@
+-- Config lightspeed.nvim
+-- nvim-spectre
+-- nvim-hlslens
+-- numb
+-- marks
+-- bufdelete
+-- toggleterm
+-- Navigator.nvim
+
+-- TODO: Look here for hlslens setup: https://github.com/kevinhwang91/nvim-hlslens/issues/17
+
+local specs_status_ok, specs = pcall(require, "specs")
+if not specs_status_ok then
+    return
+end
+
+specs.setup {
+    show_jumps = true,
+    min_jump = 10,
+    popup = {
+        delay_ms = 0, -- delay before popup displays
+        inc_ms = 10, -- time increments used for fade/resize effects
+        blend = 10, -- starting blend, between 0-100 (fully transparent), see :h winblend
+        width = 20,
+        winhl = "PMenu",
+        fader = specs.linear_fader,
+        resizer = specs.shrink_resizer,
+    },
+    ignore_filetypes = {},
+    ignore_buftypes = {
+        nofile = true,
+    },
+}
