12e546e22a35c8c08e124bda0587ff3fcd59931a
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-21 22:57:49 -0500

    Add: indentline config

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -13,6 +13,7 @@ require "user.nvim-tree"
 require "user.git"
 require "user.bars"
 require "user.togglterm"
+require "user.indentline"
 -- require("user.misc")
 
 -- FIXME: These have to be set at end because a plugin is modifying them
