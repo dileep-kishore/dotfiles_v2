6fa8a7fbf241998e3de5499a8dedab0e158ab49d
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-24 18:11:19 -0500

    Add: Custom rules to AwesomeWM config

--- private_dot_config/awesome/rules/init.lua
+++ private_dot_config/awesome/rules/init.lua
@@ -29,6 +29,7 @@ ruled.client.connect_signal("request::rules", function()
                 "Wpa_gui",
                 "veromix",
                 "xtightvncviewer",
+                "Barrier",
             },
             -- Note that the name property shown in xprop might be set slightly after creation of the client
             -- and the name shown there might not match defined rules here.
@@ -48,12 +49,42 @@ ruled.client.connect_signal("request::rules", function()
     ruled.client.append_rule {
         id = "titlebars",
         rule_any = { type = { "normal", "dialog" } },
-        properties = { titlebars_enabled = true },
+        properties = { titlebars_enabled = false },
     }
 
     -- Set Firefox to always map on the tag named '2' on screen 1.
-    -- ruled.client.append_rule {
-    --    rule       = {class = 'Firefox'},
-    --    properties = {screen = 1, tag = '2'}
-    -- }
+    ruled.client.append_rule {
+        rule = { class = "Brave-browser" },
+        properties = { screen = 1, tag = "1" },
+    }
+
+    -- Set Zoom to always appear on the tag named '5' on screen 2
+    ruled.client.append_rule {
+        rule = { class = "zoom" },
+        properties = { screen = 2, tag = "5" },
+    }
+
+    -- Set Spotify to always appear on the tag named '5' on screen 2
+    ruled.client.append_rule {
+        rule = { class = "zoom" },
+        properties = { screen = 2, tag = "6" },
+    }
+
+    -- Set Obsidian to always appear on the tag named '7' on screen 2
+    ruled.client.append_rule {
+        rule = { class = "obsidian" },
+        properties = { screen = 2, tag = "7" },
+    }
+
+    -- Set ClickUp to always appear on the tag named '8' on screen 2
+    ruled.client.append_rule {
+        rule = { class = "ClickUp" },
+        properties = { screen = 2, tag = "8" },
+    }
+
+    -- Set Zotero to always appear on the tag named '8' on screen 2
+    ruled.client.append_rule {
+        rule = { class = "Zotero" },
+        properties = { screen = 2, tag = "9" },
+    }
 end)
