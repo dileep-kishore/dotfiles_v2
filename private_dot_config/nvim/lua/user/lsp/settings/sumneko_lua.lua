4ef68b5d62fc01dda9ab13146cc56809e7f6044f
Dileep Kishore <k.dileep1994@gmail.com>
2021-12-24 13:13:51 -0500

    Add: Awesome lsp support for neovim

--- private_dot_config/nvim/lua/user/lsp/settings/sumneko_lua.lua
+++ private_dot_config/nvim/lua/user/lsp/settings/sumneko_lua.lua
@@ -3,12 +3,22 @@ return {
 
         Lua = {
             diagnostics = {
-                globals = { "vim", "awesome" },
+                enable = true,
+                globals = {
+                    -- Vim
+                    "vim",
+                    -- Awesome
+                    "awesome",
+                    "client",
+                    "root",
+                },
             },
             workspace = {
                 library = {
                     [vim.fn.expand "$VIMRUNTIME/lua"] = true,
                     [vim.fn.stdpath "config" .. "/lua"] = true,
+                    [vim.fn.expand "$VIMRUNTIME/lua/lsp"] = true,
+                    ["/usr/share/awesome/lib"] = true,
                 },
             },
         },
