a0e9b98fcf2a61a86b1163e532a95cbd551373c6
Dileep Kishore <k.dileep1994@gmail.com>
2022-01-22 13:58:21 -0500

    Add: vimclip script

---
+++ bin/vimclip.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v $EDITOR)" ]; then
+    echo 'Error: $EDITOR is not set.' >&2
+    exit 1
+fi
+
+if [ "Darwin" = $(uname -s) ]; then
+    if ! [ -x "$(command -v pbcopy)" ]; then
+        echo 'Error: pbcopy is not available.' >&2
+        exit 1
+    fi
+
+    TMP=$(mktemp -t vimclip)
+    $EDITOR $TMP
+    if [ -s $TMP ]; then
+        pbcopy <$TMP
+    else
+        rm $TMP
+    fi
+else
+    if ! [ -x "$(command -v xsel)" ]; then
+        echo 'Error: xsel is not available.' >&2
+        exit 1
+    fi
+
+    TMP=$(mktemp -p /tmp -t vimclip.XXXXXXXX)
+    $EDITOR $TMP
+    if [ -s $TMP ]; then
+        xsel -i -b <$TMP
+    else
+        rm $TMP
+    fi
+fi
