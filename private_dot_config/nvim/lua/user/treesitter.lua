1b3c851a27a9111c58330a7624af115d4d0d56f7
Dileep Kishore <k.dileep1994@gmail.com>
2022-01-22 09:13:05 -0500

    Change: Ignore ocaml treesitter parser

--- private_dot_config/nvim/lua/user/treesitter.lua
+++ private_dot_config/nvim/lua/user/treesitter.lua
@@ -8,7 +8,8 @@ end
 configs.setup {
     ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = { "" }, -- List of parsers to ignore installing
+    -- FIXME: There was a bug with this one
+    ignore_install = { "ocamllex" }, -- List of parsers to ignore installing
     highlight = {
         enable = true, -- false will disable the whole extension
         disable = { "org" }, -- list of language that will be disabled
