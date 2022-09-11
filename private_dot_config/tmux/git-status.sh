72b7205d365cb16d2c5e4cedde7377f64976438c
Dileep Kishore <k.dileep1994@gmail.com>
2022-09-11 13:46:00 -0400

    Fix: Replace egrep with grep -E

--- private_dot_config/tmux/git-status.sh
+++ private_dot_config/tmux/git-status.sh
@@ -2,7 +2,7 @@
 
 cd $1
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-STATUS=$(git status --porcelain 2>/dev/null | egrep "^(M| M)" | wc -l)
+STATUS=$(git status --porcelain 2>/dev/null | grep -E "^(M| M)" | wc -l)
 if test "$BRANCH" != ""; then
     if test "$STATUS" = "0"; then
         echo "#[fg=green,bg=#1F1F28,nobold,noitalics,nounderscore]#[fg=#1F1F28,bg=green,bold,italics]  $BRANCH #[fg=#7E9CD8,bg=green,nobold,noitalics,nounderscore]"
