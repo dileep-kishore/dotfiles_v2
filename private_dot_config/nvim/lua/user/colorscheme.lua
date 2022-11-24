6d3f5fa8c42d297a6796300afbc9167cc0af5fc4
Dileep Kishore <k.dileep1994@gmail.com>
2022-11-24 16:36:59 -0500

    Add: Neovim configuration for catppuccin theme

--- private_dot_config/nvim/lua/user/colorscheme.lua
+++ private_dot_config/nvim/lua/user/colorscheme.lua
@@ -5,13 +5,41 @@ vim.g.tokyonight_style = "night"
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 -- load the colorscheme
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 local status_ok, catppuccin = pcall(require, "catppuccin")
 if not status_ok then
     return
 end
 
-catppuccin.setup()
+catppuccin.setup({
+    flavor = "mocha", -- latte, frappe, macchiato, mocha
+    dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.10,
+    },
+    integrations = {
+        gitsigns = true,
+        leap = true,
+        mason = true,
+        neogit = true,
+        cmp = true,
+        nvimtree = true,
+        treesitter = true,
+        treesitter_context = true,
+        telescope = true,
+        which_key = true,
+        dap = {
+            enabled = true,
+        },
+        indent_blankline = {
+            enabled = true,
+            colored_indent_levels = false,
+        },
+        native_lsp = {
+            enabled = true,
+        }
+    }
+})
 vim.cmd [[colorscheme catppuccin]]
 
 -- scrollbar
